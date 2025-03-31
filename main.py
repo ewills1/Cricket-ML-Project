626a30ee77e1a96c353acf812f9d7358b9279fcf
Edward Wills <ewills1@sheffield.ac.uk>
2025-03-31 15:53:35 +0100

    Updated main, removed unnecessary class

--- main.py
+++ main.py
@@ -1,26 +1,19 @@
 from train import TrainModel
 from parse.parse_json import ParseJson
 
-class Main:
-    def __init__(self, json_dir, csv_dir):
-        self.json_dir = json_dir
-        self.csv_dir = csv_dir
-
-    def run(self):
-        # Parse JSON files and save to CSV
-        parser = ParseJson(self.json_dir)
-        parser.parse_json_files()
-        
-        # Train the model using the parsed data
-        trainer = TrainModel(self.csv_dir)
-        trainer.train_model()
-        trainer.plot_results()
+def main(json_dir, csv_dir):
+    # Parse JSON files and save to CSV
+    parser = ParseJson(json_dir)
+    parser.parse_json_files()
+    
+    # Train the model using the parsed data
+    trainer = TrainModel(csv_dir)
+    trainer.train_model()
+    trainer.plot_results()
 
 if __name__ == "__main__":
     # Directories for JSON and CSV files
     json_dir = "./data/ipl" 
     csv_dir = "./csv/" 
-    
 
-    main = Main(json_dir, csv_dir)
-    main.run()
+    main(json_dir, csv_dir)
